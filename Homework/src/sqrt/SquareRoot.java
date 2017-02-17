5a25eb40848827a622e581b61684f657148cc128
kurt <kurt@kurt-ThinkPad-T440>
2017-02-17 13:01:08 -0500

    Comments/Constructors
    entered java doc comments and created multiple constructors

--- Homework/src/sqrt/SquareRoot.java
+++ Homework/src/sqrt/SquareRoot.java
@@ -1,5 +1,42 @@
 package sqrt;
 
 public class SquareRoot {
-
+	
+	private double input;
+	private double error = .1;
+	//Default constructor
+	public SquareRoot(){
+		
+	}
+	/**
+	 * Constructor that takes in number
+	 * @param num
+	 */
+	public SquareRoot(double num){
+		this.input = num;
+	}
+	/**
+	 * Constructor that takes in number and margin of error
+	 * @param num
+	 * @param err
+	 */
+	public SquareRoot(double num, double err){
+		this.input = num;
+		this.error = err;
+	}
+	//Getters and Setters
+	public double getInput() {
+		return input;
+	}
+	public void setInput(double input) {
+		this.input = input;
+	}
+	public double getError() {
+		return error;
+	}
+	public void setError(double error) {
+		this.error = error;
+	}
+	
+	
 }
