ccc54b92062e3cd973e8b22f3dec4a4466222e9d
kurt <kurt@kurt-ThinkPad-T440>
2017-02-17 14:02:17 -0500

    Working Method
    Working square root method with the exception of still printing out
    location with the approx sqrt value. Ready to debug.

--- Homework/src/sqrt/SquareRoot.java
+++ Homework/src/sqrt/SquareRoot.java
@@ -1,42 +1,94 @@
 package sqrt;
 
 public class SquareRoot {
-	
+
 	private double input;
-	private double error = .1;
-	//Default constructor
-	public SquareRoot(){
-		
+	private double error;
+	private double guess;
+
+	// Default constructor
+	public SquareRoot() {
+
 	}
+
 	/**
 	 * Constructor that takes in number
+	 * 
 	 * @param num
 	 */
-	public SquareRoot(double num){
+	public SquareRoot(double num) {
 		this.input = num;
+		this.error = .1;
+		this.setGuess(num / 2);
+
+		System.out.print(sqrt(this.input, this.guess, this.error));
+
 	}
+
 	/**
 	 * Constructor that takes in number and margin of error
+	 * 
 	 * @param num
 	 * @param err
 	 */
-	public SquareRoot(double num, double err){
+	public SquareRoot(double num, double err) {
 		this.input = num;
 		this.error = err;
+		this.guess = num / 2;
+
+		System.out.print(sqrt(this.input, this.guess, this.error));
 	}
-	//Getters and Setters
+
+	// Getters and Setters
 	public double getInput() {
 		return input;
 	}
+
 	public void setInput(double input) {
 		this.input = input;
 	}
+
 	public double getError() {
 		return error;
 	}
+
 	public void setError(double error) {
 		this.error = error;
 	}
-	
-	
+
+	public double getGuess() {
+		return guess;
+	}
+
+	public void setGuess(double guess) {
+		this.guess = guess;
+	}
+
+	/**
+	 * method to calculate square root
+	 * 
+	 * @param num
+	 * @param guess
+	 * @param err
+	 * @return approximate square root of input value
+	 */
+	public static double sqrt(double num, double guess, double err) {
+		// if the calculated square root is within the error will return guess
+		if (Math.abs(guess * guess - num) <= err)
+			return guess;
+		// if the calculated square root is outside of the acceptable error
+		// recursively runs method
+		else {
+			guess = (guess * guess + num) / (2 * guess);
+		}
+		return (sqrt(num, guess, err));
+
+	}
+
+	public static void main(String[] args) {
+		SquareRoot test = new SquareRoot(80);
+
+		System.out.print(test);
+	}
+
 }
