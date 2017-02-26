fb48d0e26285f06bf4f71bcfa4e76ea36f734768
kurt <kurt@kurt-ThinkPad-T440>
2017-02-26 08:33:28 -0500

    Java doc comments and tests for each method

--- Homework/src/sqrt/SquareRoot.java
+++ Homework/src/sqrt/SquareRoot.java
@@ -24,7 +24,15 @@ public class SquareRoot {
 
 	}
 
+	/**
+	 * method to calculate square root
+	 * 
+	 * @param num
+	 * @param err
+	 * @return approximate square root of input value
+	 */
 	public static double sqrt(double num, double err) {
+		// sets guess to half input value
 		double guess = num / 2;
 		// if the calculated square root is within the error will return guess
 		if (Math.abs(guess * guess - num) <= err)
@@ -38,8 +46,16 @@ public class SquareRoot {
 
 	}
 
+	/**
+	 * method to calculate square root
+	 * 
+	 * @param num
+	 * @return approximate square root of input value
+	 */
 	public static double sqrt(double num) {
 
+		// sets guess to half the input value
+		// sets error value
 		double guess = num / 2;
 		double err = .1;
 
@@ -56,8 +72,15 @@ public class SquareRoot {
 	}
 
 	public static void main(String[] args) {
-
-		System.out.print(sqrt(9, 3, .01));
+		
+		//tests method that take 3 parameters: input, guess, and error
+		System.out.println(sqrt(9, 3, .01));
+		
+		//tests method that takes 2 parameters: input and error
+		System.out.println(sqrt(6,.001));
+		
+		//tests method that takes 1 paramter: input
+		System.out.println(sqrt(25));
 	}
 
 }
