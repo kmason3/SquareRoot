dab73d0e4571fe8c4b8f8c6a655915fa3cfa3a89
kurt <kurt@kurt-ThinkPad-T440>
2017-03-13 05:31:25 -0400

    Made constructors and debugged program

--- Homework/src/sqrt/SquareRoot.java
+++ Homework/src/sqrt/SquareRoot.java
@@ -2,6 +2,29 @@ package sqrt;
 
 public class SquareRoot {
 
+	private double input;
+	private double error = .1;
+
+	/**
+	 * Constructor with user defined input
+	 * 
+	 * @param in
+	 */
+	public SquareRoot(double in) {
+		this.input = in;
+	}
+
+	/**
+	 * Constructor with user defined input and error
+	 * 
+	 * @param in
+	 * @param er
+	 */
+	public SquareRoot(double in, double er) {
+		this.input = in;
+		this.error = er;
+	}
+
 	/**
 	 * method to calculate square root
 	 * 
@@ -25,45 +48,23 @@ public class SquareRoot {
 	}
 
 	/**
-	 * method to calculate square root
+	 * method to calculate square root using a user defined object
 	 * 
-	 * @param num
-	 * @param err
-	 * @return approximate square root of input value
+	 * @return approximate square root
 	 */
-	public static double sqrt(double num, double err) {
-		// sets guess to half input value
-		double guess = num / 2;
-		// if the calculated square root is within the error will return guess
-		if (Math.abs(guess * guess - num) <= err)
-			return guess;
-		// if the calculated square root is outside of the acceptable error
-		// recursively runs method
-		else {
-			guess = (guess * guess + num) / (2 * guess);
-		}
-		return (sqrt(num, guess, err));
-
-	}
-
-	/**
-	 * method to calculate square root
-	 * 
-	 * @param num
-	 * @return approximate square root of input value
-	 */
-	public static double sqrt(double num) {
+	public double sqrt() {
 
 		// sets guess to half the input value
 		// sets error value
-		double guess = num / 2;
-		double err = .1;
+		double guess = this.input / 2;
+		double num = this.input;
+		double err = this.error;
 
 		// if the calculated square root is within the error will return guess
 		if (Math.abs(guess * guess - num) <= err)
 			return guess;
 		// if the calculated square root is outside of the acceptable error
-		// recursively runs method
+		// recursively runs method with new guess
 		else {
 			guess = (guess * guess + num) / (2 * guess);
 		}
@@ -72,15 +73,18 @@ public class SquareRoot {
 	}
 
 	public static void main(String[] args) {
-		
-		//tests method that take 3 parameters: input, guess, and error
+
+		// tests method that take 3 parameters: input, guess, and error
 		System.out.println(sqrt(9, 3, .01));
-		
-		//tests method that takes 2 parameters: input and error
-		System.out.println(sqrt(6,.001));
-		
-		//tests method that takes 1 paramter: input
-		System.out.println(sqrt(25));
+		// creates object with input 3
+		SquareRoot test = new SquareRoot(3);
+		// tests sqrt method on 3 with default error
+		System.out.println(test.sqrt());
+		// creates object with input 3 and error .01
+		SquareRoot test2 = new SquareRoot(3, .01);
+		// tests sqrt method with 3 and user defined error
+		System.out.println(test2.sqrt());
+
 	}
 
 }
